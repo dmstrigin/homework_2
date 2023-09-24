eebd2e5f728d5bbbd104f59e7b775cad6ba9b121
dmstrigin <dmstrigin@yandex.ru>
2023-09-24 09:43:39 +0300

    finish homework2

---
+++ Documents/Git/Homework/Homework2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру 
+// этого числа.
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
+
+Console.WriteLine("Enter your number");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("second number -> "+stringNumber[1]);
+
